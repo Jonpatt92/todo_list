b9b961d9189906ffffddfd95cbcb119e0a9e2cd4
Jon Patterson <JonPatt92@gmail.com>
2020-03-10 16:51:03 -0600

    Create HomepageController with index action to display the app's homepage at the `/` URI

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+      return $this->render('homepage/index.html.twig');
+    }
+}
