de1a472d1e5242a421d89ee6fb286ffb74fc17f6
Jon Patterson <JonPatt92@gmail.com>
2020-03-10 13:28:29 -0600

    Add hidden 'status' field with default value of 'Incomplete' for new Todo record form

--- src/Form/Type/TodoType.php
+++ src/Form/Type/TodoType.php
@@ -4,6 +4,7 @@ namespace App\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use App\Entity\Todo;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,6 +15,9 @@ class TodoType extends AbstractType
     {
         $builder
             ->add('task', TextType::class)
+            ->add('status', HiddenType::class, [
+              'data' => 'Incomplete'
+            ])
             ->add('save', SubmitType::class)
         ;
     }
